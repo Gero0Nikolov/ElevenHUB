f047f02708eb7b39fce9321fec6a62b06dd6e0be
Gero Nikolov <vtm.sunrise@gmail.com>
2017-11-18 04:13:34 +0200

    Brother Framework bug fix; Fixed in the Dev Pack v1.0; Hubby plugin updated

--- wp-content/plugins/hello-hubby/hello-hubby.php
+++ wp-content/plugins/hello-hubby/hello-hubby.php
@@ -17,83 +17,86 @@ class HELLO_HUBBY {
 	}
 
 	function say_hello() {
-		$user_id = get_current_user_id();
-		$short_name = get_user_meta( $user_id, "user_shortname", true );
-		if ( empty( $short_name ) || !$short_name ) { $first_name = get_user_meta( $user_id, "first_name", true ); }
+		if ( !wp_is_mobile() ) {
+			$user_id = get_current_user_id();
+			$short_name = get_user_meta( $user_id, "user_shortname", true );
+			if ( empty( $short_name ) || !$short_name ) { $first_name = get_user_meta( $user_id, "first_name", true ); }
 
-		$name_ = empty( $short_name ) || !$short_name ? $first_name : $short_name;
+			$name_ = empty( $short_name ) || !$short_name ? $first_name : $short_name;
 
-		echo "
-		<script type='text/javascript'>
-			jQuery( document ).ready( function(){
-				view_ = \"\
-				<span class='bull-separator'>&bull;</span>\
-				<div class='hubby-container'>\
-					<img class='hubby' src='". get_template_directory_uri() ."/assets/fonts/emojies-all/1f917.png' />\
-					<div class='hubby-box'>Hello, $name_</div>\
-				</div>\
-				\";
-				jQuery( '#site-navigation .left-aligned' ).append( view_ );
-			} );
-		</script>
-		<style>
-		.hubby {
-			display: inline-block;
-			width: 100%;
-			vertical-align: middle;
-			transform: translateZ(0);
-			webkit-transition: all 0.25s ease-in-out;
-			-moz-transition: all 0.25s ease-in-out;
-			-o-transition: all 0.25s ease-in-out;
-			transition: all 0.25s ease-in-out;
-		}
-		.hubby:hover {
-			transform: translateZ(0) scale(1.5);
-		}
+			echo "
+			<script type='text/javascript'>
+				jQuery( document ).ready( function(){
+					view_ = \"\
+					<span class='bull-separator'>&bull;</span>\
+					<div class='hubby-container'>\
+						<img class='hubby' src='". get_template_directory_uri() ."/assets/fonts/emojies-all/1f917.png' />\
+						<div class='hubby-box'>Hello, $name_</div>\
+					</div>\
+					\";
+					jQuery( '#site-navigation .left-aligned' ).append( view_ );
+				} );
+			</script>
+			<style>
+			.hubby {
+				display: inline-block;
+				width: 100%;
+				vertical-align: middle;
+				transform: translateZ(0);
+				webkit-transition: all 0.25s ease-in-out;
+				-moz-transition: all 0.25s ease-in-out;
+				-o-transition: all 0.25s ease-in-out;
+				transition: all 0.25s ease-in-out;
+			}
+			.hubby:hover {
+				transform: translateZ(0) scale(1.5);
+			}
 
-		.hubby-container {
-			display: inline-block;
-			vertical-align: middle;
-			width: 15%;
-		}
-		.hubby-container:hover .hubby-box { opacity: 1; }
+			.hubby-container {
+				display: inline-block;
+				vertical-align: middle;
+				width: 32px;
+				position: relative;
+			}
+			.hubby-container:hover .hubby-box { opacity: 1; }
 
-		.hubby-box {
-			position: absolute;
-			background: rgba(0, 0, 0, 0.65);
-			right: 0;
-			bottom: 0;
-			transform: translateY(115%);
-			font-family: PlayFairRegular;
-			font-size: 1rem;
-			font-weight: normal;
-			color: #fff;
-			padding: 0.25em 0.75em;
-			border-radius: 5px;
-			opacity: 0;
-			webkit-transition: all 0.25s ease-in-out;
-			-moz-transition: all 0.25s ease-in-out;
-			-o-transition: all 0.25s ease-in-out;
-			transition: all 0.25s ease-in-out;
+			.hubby-box {
+				position: absolute;
+				background: rgba(0, 0, 0, 0.85);
+				right: 0;
+				bottom: 0;
+				transform: translateY(145%);
+				font-family: PlayFairRegular;
+				font-size: 1rem;
+				font-weight: normal;
+				color: #fff;
+				padding: 0.25em 0.75em;
+				border-radius: 5px;
+				opacity: 0;
+				min-width: 120px;
+				text-align: center;
+				webkit-transition: all 0.25s ease-in-out;
+				-moz-transition: all 0.25s ease-in-out;
+				-o-transition: all 0.25s ease-in-out;
+				transition: all 0.25s ease-in-out;
+			}
+			.hubby-box:after {
+				bottom: 100%;
+				right: 7.5px;
+				border: solid transparent;
+				content: ' ';
+				height: 0;
+				width: 0;
+				position: absolute;
+				pointer-events: none;
+				border-color: rgba(136, 183, 213, 0);
+				border-bottom-color: rgba(0, 0, 0, 0.85);
+				border-width: 10px;
+				margin-left: -10px;
+			}
+			</style>
+			";
 		}
-		.hubby-box:after {
-			bottom: 100%;
-			right: 25%;
-			border: solid transparent;
-			content: ' ';
-			height: 0;
-			width: 0;
-			position: absolute;
-			pointer-events: none;
-			border-color: rgba(136, 183, 213, 0);
-			border-bottom-color: rgba(0, 0, 0, 0.65);
-			border-width: 10px;
-			margin-left: -10px;
-		}
-
-		#site-navigation .left-aligned { position: relative; }
-		</style>
-		";
 	}
 }
 ?>
